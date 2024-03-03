b34f1141f3fc65f26692af59cd71a9844e06ff4b
unknown <narutoronaldo69@mail.com>
2024-03-03 14:24:57 +0300

    Java is hard at first

---
+++ Vars.java
@@ -0,0 +1,6 @@
+public class Vars {
+    public static void main(String[] args) {
+        var A=20
+        System.out.println("Hello, World!"+ A);
+    }
+}
---
+++ Yahye.java
@@ -0,0 +1,8 @@
+public class Yahye {
+    public static void main(String[] args) {
+        System.out.println("Yahye is sick today");
+        System.out.println("He can go and sleep");
+
+
+    }
+}
